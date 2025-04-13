a4cee84b50bf0a87890c511475399e791591206d
EsperAdmin <dedicated@internetstuff.xyz>
2025-04-12 19:20:40 -0700

    remove text content

--- js.js
+++ js.js
@@ -8,7 +8,6 @@ function createDivs () {
     for (i = 0; i < 16; i++) {
         const content = document.createElement("div");
         content.classList.add("content");
-        content.textContent = "div"
 
         container.appendChild(content);
     }
