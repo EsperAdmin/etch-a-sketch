13c1f4be62787409d333997c1c0cd57726b36c1e
EsperAdmin <dedicated@internetstuff.xyz>
2025-04-15 21:58:23 -0700

    make grid calculate and set cube size

--- js.js
+++ js.js
@@ -1,16 +1,32 @@
 const container = document.querySelector("#container");
 
-createDivs();
+let getGridSize = 16 * 16;
+let getGridSizeRaw = 16;
+createDivs(getGridSize, getGridSizeRaw);
 
 const contentSelect = document.querySelectorAll(".content")
 contentSelect.forEach(contentSelect => contentSelect.addEventListener("mouseenter", (event) => {event.target.style.backgroundColor = "orange";}));
 
 
-function createDivs () {
-    for (i = 0; i < 16; i++) {
+
+
+
+function createDivs (gridSize, getGridSizeRaw) {
+    for (i = 0; i < gridSize; i++) {
+        let calculateGrid = (796 / getGridSizeRaw);
+
         const content = document.createElement("div");
+        // content.style.height = `${calculateGrid}`;
+        // content.style.flexBasis = `${calculateGrid}`;
+        // content.style.color = "red";
+
+
         content.classList.add("content");
 
         container.appendChild(content);
+
+        let root = document.documentElement;
+        root.style.setProperty("--basis", calculateGrid + "px");
+        root.style.setProperty("--height", calculateGrid + "px");
     }
 }
